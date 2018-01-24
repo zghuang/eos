2aaa134ef28950849bf3365f559348d4053812a7
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2018-01-24 16:55:00 -0500

    Don't allow odd batch sizes for txn_test_gen

--- plugins/txn_test_gen_plugin/txn_test_gen_plugin.cpp
+++ plugins/txn_test_gen_plugin/txn_test_gen_plugin.cpp
@@ -215,6 +215,8 @@ struct txn_test_gen_plugin_impl {
          throw fc::exception(fc::invalid_operation_exception_code);
       if(batch_size < 1 || batch_size > 250)
          throw fc::exception(fc::invalid_operation_exception_code);
+      if(batchsize & 1)
+         throw fc::exception(fc::invalid_operation_exception_code);
 
       running = true;
 
